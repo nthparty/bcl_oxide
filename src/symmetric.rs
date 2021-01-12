6ec7146309c95a96aa22d65fb54b99a64ca546b9
bengetch <bengetch@gmail.com>
2021-01-12 12:26:46 -0500

    Fmt

--- src/symmetric.rs
+++ src/symmetric.rs
@@ -5,23 +5,13 @@ pub struct Symmetric {}
 
 impl Symmetric {
 
-    pub fn secret() -> secretbox::Key {
-        secretbox::gen_key()
-    }
+    pub fn secret() -> secretbox::Key { secretbox::gen_key() }
 
-    pub fn nonce() -> secretbox::Nonce {
-        secretbox::gen_nonce()
-    }
+    pub fn nonce() -> secretbox::Nonce { secretbox::gen_nonce() }
 
     pub fn encrypt(msg: &[u8], n: &secretbox::Nonce, k: &secretbox::Key)
-               -> Vec<u8>
-    {
-        secretbox::seal(msg, n, k)
-    }
+               -> Vec<u8> { secretbox::seal(msg, n, k) }
 
     pub fn decrypt(ct: &[u8], n: &secretbox::Nonce, k: &secretbox::Key)
-               -> Result<Vec<u8>, ()>
-    {
-        secretbox::open(ct, n, k)
-    }
+               -> Result<Vec<u8>, ()> { secretbox::open(ct, n, k) }
 }
